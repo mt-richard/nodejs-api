aea4dc26e3deb6dae359cac33d004feda85adfa3
mt-richard <Richard@festimoltd.com>
2023-05-03 23:54:27 +0200

    token with JWT

--- app.js
+++ app.js
@@ -1,11 +1,52 @@
 const express = require("express")
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const app = express()
+
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const secretKey = 'mysecretkey';
+
+function authenticate(req, res, next) {
+//   const authHeader = req.headers.authorization;
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Authorization header missing' });
+  }
+//   const token = authHeader.split(' ')[1];
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Token is not valid' });
+    }
+    req.user = decoded.user;
+    next();
+  });
+}
+
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    if (username === 'admin' && password === 'admin') {
+      const token = jwt.sign({ user: { username } }, secretKey, {expiresIn: '5m'});
+    //  console.log({ token });
+    res.cookie('token', token, { httpOnly: true, maxAge: 3600000 });
+    res.json({ token });
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' });
+    }
+  });
+  
+  app.get('/protected', authenticate, (req, res) => {
+    res.json({ message: `Hello, ${req.user.username}!` });
+  });
+
 app.use('/users', require('./routes/users.js'))
 app.get('/', function(req, res) {
-    res.send("Working great")
+    res.sendFile(__dirname + '/views/index.html')
 });
 
 
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ const { Route } = require('react-router-dom')
 const route = express.Router()
 const app =express()
 const { users } = require('../models/index.js')
+const { col } = require('sequelize')
+
 
 route.get('/', async(req, res) =>{
     const alluser = await users.findAll();
@@ -16,6 +18,76 @@ route.post('/add', async(req, res) =>{
         password:req.body.password } 
     const user = await users.create(data);
     res.json(user)
-})
+});
+
+route.delete('/delete/:id', async(req, res) => {
+    const userid = req.params.id;
+    const selecteduser = await users.findByPk(userid).then(
+        user => {
+            if (!user) {
+                res.sendStatus(404)
+            } else {
+                user.destroy()
+                res.send("User deleted.....")
+            }
+    })
+});
+
+route.put('/edit/:id', async(req, res) => {
+    const userid = req.params.id;
+    const { name, email, username, password} =req.body
+    const getuser = await users.findByPk(userid)
+        .then(user =>{
+            if (!user) {
+                res.sendStatus(404)
+            } else {
+                    user.name = name
+                    user.email = email 
+                    user.username = username
+                    user.password = password
+                    user.save()
+                    res.send({"message":"User Update", "user": user})
+                    // console.log({"message":"User Update", "user": user})
+                    
+            }
+        })
+    // res.send(getuser);
+});
+
+
+
+route.get('/:id', async(req, res) => {
+    const userid = req.params.id;
+    const getuser = await users.findByPk(userid)
+    res.send(getuser);
+});
+// app.put('/:id', (req, res) => {
+//     const id = req.params.id
+//     const { name, email, password } = req.body;
+//     User.findByPk(id)
+//       .then(user => {
+//         if (!user) {
+//           res.sendStatus(404); 
+//         } else {
+//           user.name = name;
+//           user.email = email;
+//           user.password = password;
+//           user.save()
+//             .then(() => {
+//               console.log(`User ${id} updated`);
+//               res.sendStatus(204); 
+//             })
+//             .catch(err => {
+//               console.error(err);
+//               res.sendStatus(500); 
+//             });
+//         }
+//       })
+//       .catch(err => {
+//         console.error(err);
+//         res.sendStatus(500); 
+//       });
+//   });
+  
 
 module.exports = route
